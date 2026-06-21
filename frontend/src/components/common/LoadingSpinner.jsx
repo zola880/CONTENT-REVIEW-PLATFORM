8fc9f90be6ae594823b590b11504fff22b106ce1
zola880 <zelalemybabe77@gmail.com>
2026-06-21 03:09:43 +0300

    feat(components): add LoadingSpinner

---
+++ frontend/src/components/common/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => (
+  <div className="flex justify-center items-center h-32">
+    <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
+  </div>
+);
+
+export default LoadingSpinner;
