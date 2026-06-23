d98e517a546b49459b293da3da61c18430e6acfc
zola880 <zelalemybabe77@gmail.com>
2026-06-23 05:49:07 +0300

    hg

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,46 @@
 const app = require('./src/app');
 const connectDB = require('./src/config/database');
-const { port } = require('./src/config/env');
+const { port, nodeEnv } = require('./src/config/env');
 const logger = require('./src/utils/logger');
 
 // Connect to MongoDB
 connectDB();
 
 const server = app.listen(port, () => {
-  logger.info(`Server running on port ${port} in ${process.env.NODE_ENV} mode`);
+  logger.info(` Server running on port ${port} in ${nodeEnv} mode`);  
 });
 
-// Graceful shutdown
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM signal received: closing HTTP server');
+
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  logger.error(' UNHANDLED REJECTION! Shutting down...');
+  logger.error(err.stack);
   server.close(() => {
-    logger.info('HTTP server closed');
+    process.exit(1);
+  });
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+  logger.error(' UNCAUGHT EXCEPTION! Shutting down...');
+  logger.error(err.stack);
+  process.exit(1);
+});
+
+// ----- Graceful Shutdown -----
+process.on('SIGTERM', () => {
+  logger.info(' SIGTERM signal received: closing HTTP server gracefully');
+  server.close(() => {
+    logger.info(' HTTP server closed');
     process.exit(0);
   });
-});// Handle unhandled promise rejections
+});
+
+process.on('SIGINT', () => {
+  logger.info(' SIGINT signal received: closing HTTP server gracefully');
+  server.close(() => {
+    logger.info(' HTTP server closed');
+    process.exit(0);
+  });
+});
